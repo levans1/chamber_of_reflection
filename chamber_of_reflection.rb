78776c5785f1fac108b6877429a86d20e6289046
levans1 <45011820+levans1@users.noreply.github.com>
2020-01-21 11:14:43 -0800

    Add files via upload

--- chamber_of_reflection.rb
+++ chamber_of_reflection.rb
@@ -1,27 +1,29 @@
 # Welcome to Sonic Pi v3.
 use_bpm 131
 use_synth :piano
-play :g5
-sleep 1
-play :e5
-sleep 1
-play :b4
-sleep 1.5
-play :c5
-sleep 0.25
-play :b4
-sleep 0.25
-play :a4
-sleep 2
-play :r
-sleep 2
-play :b4
-sleep 1
-play :c5
-sleep 1
-play :d5
-sleep 1.5
-play :b4
-sleep 0.5
-play :f4
-sleep 2
+live_loop :demarco do
+  play :g5
+  sleep 1
+  play :e5
+  sleep 1
+  play :b4
+  sleep 1.5
+  play :c5
+  sleep 0.25
+  play :b4
+  sleep 0.25
+  play :a4
+  sleep 2
+  play :r
+  sleep 2
+  play :b4
+  sleep 1
+  play :c5
+  sleep 1
+  play :d5
+  sleep 1.5
+  play :b4
+  sleep 0.5
+  play :f4
+  sleep 2
+end
